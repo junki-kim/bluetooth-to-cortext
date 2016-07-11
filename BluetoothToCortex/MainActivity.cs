662b744a9e3c76444c47888a2d4ccdfd89d7a27b
kj0604 <kj0604@naver.com>
2016-07-11 23:11:47 +0900

    implemented bluetooth device scanning

--- BluetoothToCortex/MainActivity.cs
+++ BluetoothToCortex/MainActivity.cs
@@ -6,16 +6,22 @@ using Android.Views;
 using Android.Widget;
 using Android.OS;
 using Android.Bluetooth;
+using Android.Util;
 
 namespace BluetoothToCortex
 {
-    [Activity(Label = "BluetoothToCortex", MainLauncher = true, Icon = "@drawable/icon")]
+    [Activity(Label = "@string/ApplicationName", MainLauncher = true, Icon = "@drawable/icon")]
     public class MainActivity : Activity
     {
         // Local Bluetooth adapter
         private BluetoothAdapter mBluetoothAdapter = null;
+        private static ArrayAdapter<string> pairedDevicesArrayAdapter;
+        private static ArrayAdapter<string> newDevicesArrayAdapter;
+        private BTReceiver receiver;
+
         private Button mBtBtn = null;
         private ListView mDeviceListView = null;
+
 
         protected override void OnCreate(Bundle bundle)
         {
@@ -28,10 +34,29 @@ namespace BluetoothToCortex
             // Get local Bluetooth adapter
             mBluetoothAdapter = BluetoothAdapter.DefaultAdapter;
 
+            // Initialize array adapters. One for already paired devices and
+            // one for newly discovered devices
+            pairedDevicesArrayAdapter = new ArrayAdapter<string>(this, Resource.Layout.device_name);
+            newDevicesArrayAdapter = new ArrayAdapter<string>(this, Resource.Layout.device_name);
+
             // Get our button from the layout resource,
             // and attach an event to it
             mBtBtn = FindViewById<Button>(Resource.Id.FindBtButton);
+            //mDeviceListView = FindViewById<ListView>(Resource.Id.deviceListView);
+
+
+            // Find and setup list view
             mDeviceListView = FindViewById<ListView>(Resource.Id.deviceListView);
+            mDeviceListView.Adapter = newDevicesArrayAdapter;
+            // TODO Implement click event function
+            //mDeviceListView.ItemClick += devicelistClick;
+
+            
+            // Register for broadcasts when a device is discovered
+            receiver = new BTReceiver(this);
+            var filter = new IntentFilter(BluetoothDevice.ActionFound);
+            RegisterReceiver(receiver, filter);
+            
 
             //button.Click += delegate { button.Text = string.Format("{0} clicks!", count++); };
             mBtBtn.Click += delegate
@@ -50,7 +75,69 @@ namespace BluetoothToCortex
                     Toast.MakeText(this, "Bluetooth adapter is not enabled.", ToastLength.Long).Show();
                     return;
                 }
+
+                DoDiscovery();
             };
+        }
+
+        /// <summary>
+		/// Start device discover with the BluetoothAdapter
+		/// </summary>
+		private void DoDiscovery()
+        {
+            // Indicate scanning in the title
+            SetProgressBarIndeterminateVisibility(true);
+            SetTitle(Resource.String.scanning);
+
+            // Turn on sub-title for new devices
+            //FindViewById<View>(Resource.Id.title_new_devices).Visibility = ViewStates.Visible;
+
+            // If we're already discovering, stop it
+            if (mBluetoothAdapter.IsDiscovering)
+            {
+                mBluetoothAdapter.CancelDiscovery();
+            }
+
+            // Request discover from BluetoothAdapter
+            mBluetoothAdapter.StartDiscovery();
+        }
+
+        public class BTReceiver : BroadcastReceiver
+        {
+            Activity _sender;
+
+            public BTReceiver(Activity sender)
+            {
+                _sender = sender;
+            }
+
+            public override void OnReceive(Context context, Intent intent)
+            {
+                string action = intent.Action;
+
+                // When discovery finds a device
+                if (action == BluetoothDevice.ActionFound)
+                {
+                    // Get the BluetoothDevice object from the Intent
+                    BluetoothDevice device = (BluetoothDevice)intent.GetParcelableExtra(BluetoothDevice.ExtraDevice);
+                    // If it's already paired, skip it, because it's been listed already
+                    if (device.BondState != Bond.Bonded)
+                    {
+                        newDevicesArrayAdapter.Add(device.Name + "\n" + device.Address);
+                    }
+                    // When discovery is finished, change the Activity title
+                }
+                else if (action == BluetoothAdapter.ActionDiscoveryFinished)
+                {
+                    _sender.SetProgressBarIndeterminateVisibility(false);
+                    _sender.SetTitle(Resource.String.ApplicationName);
+                    if (newDevicesArrayAdapter.Count == 0)
+                    {
+                        var noDevices = _sender.Resources.GetText(Resource.String.none_found).ToString();
+                        newDevicesArrayAdapter.Add(noDevices);
+                    }
+                }
+            }
         }
     }
 }
--- BluetoothToCortex/Resources/Resource.Designer.cs
+++ BluetoothToCortex/Resources/Resource.Designer.cs
@@ -80,7 +80,10 @@ namespace BluetoothToCortex
 		{
 			
 			// aapt resource value: 0x7f030000
-			public const int Main = 2130903040;
+			public const int device_name = 2130903040;
+			
+			// aapt resource value: 0x7f030001
+			public const int Main = 2130903041;
 			
 			static Layout()
 			{
@@ -100,6 +103,51 @@ namespace BluetoothToCortex
 			
 			// aapt resource value: 0x7f040000
 			public const int FindBt = 2130968576;
+			
+			// aapt resource value: 0x7f040004
+			public const int bt_not_enabled_leaving = 2130968580;
+			
+			// aapt resource value: 0x7f04000e
+			public const int button_scan = 2130968590;
+			
+			// aapt resource value: 0x7f04000f
+			public const int connect = 2130968591;
+			
+			// aapt resource value: 0x7f040010
+			public const int discoverable = 2130968592;
+			
+			// aapt resource value: 0x7f04000b
+			public const int none_found = 2130968587;
+			
+			// aapt resource value: 0x7f04000a
+			public const int none_paired = 2130968586;
+			
+			// aapt resource value: 0x7f040003
+			public const int not_connected = 2130968579;
+			
+			// aapt resource value: 0x7f040008
+			public const int scanning = 2130968584;
+			
+			// aapt resource value: 0x7f040009
+			public const int select_device = 2130968585;
+			
+			// aapt resource value: 0x7f040002
+			public const int send = 2130968578;
+			
+			// aapt resource value: 0x7f040006
+			public const int title_connected_to = 2130968582;
+			
+			// aapt resource value: 0x7f040005
+			public const int title_connecting = 2130968581;
+			
+			// aapt resource value: 0x7f040007
+			public const int title_not_connected = 2130968583;
+			
+			// aapt resource value: 0x7f04000d
+			public const int title_other_devices = 2130968589;
+			
+			// aapt resource value: 0x7f04000c
+			public const int title_paired_devices = 2130968588;
 			
 			static String()
 			{
